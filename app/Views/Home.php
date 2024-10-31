6fa9166c20201e7215f9430aef3b7ead8352e219
TordiThor11 <valetordi@gmail.com>
2024-10-31 15:13:50 -0300

    Correccion de las cartas, ahora se muestran 3 por fila.

--- app/Views/Home.php
+++ app/Views/Home.php
@@ -35,19 +35,23 @@
     <div class="container mt-5">
         <div class="row">
             <!-- Ejemplo de carta de proyecto -->
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Proyecto 1">
-                    <div class="card-body">
-                        <h5 class="card-title">Proyecto 1</h5>
-                        <p class="card-text">Descripción breve del proyecto.</p>
-                        <a href="#" class="btn btn-primary">Ver más</a>
+            <?php foreach ($proyectos as $proyecto): ?>
+                <div class="col-md-4">
+
+                    <div class="card">
+                        <img src="https://via.placeholder.com/150" class="card-img-top" alt="<?= $proyecto['nombre']; ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $proyecto['nombre']; ?></h5>
+                            <p class="card-text"><?= $proyecto['fecha_limite']; ?></p>
+                            <a href="#" class="btn btn-primary">Ver más</a>
+                        </div>
                     </div>
+
                 </div>
-            </div>
-            <!-- Repite la estructura de la carta de proyecto para otros proyectos -->
+            <?php endforeach; ?>
         </div>
     </div>
+
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
