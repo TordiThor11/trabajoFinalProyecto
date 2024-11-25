7447ddb777b8c5711801797283e9c19d7c69e62f
desertfoxs <angelolivetti78@gmail.com>
2024-11-25 17:44:54 -0300

    Arreglos esteticos en el footer

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,42 +1,19 @@
-    <!-- Footer -->
-    <footer class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="text-center">
                     <h5>Sobre Nosotros</h5>
                     <p>Una plataforma para compartir y descubrir proyectos creativos.</p>
-                </div>
-                <div class="col-md-4">
-                    <h5>Enlaces Rápidos</h5>
-                    <ul class="list-unstyled">
-                        <li><a href="#" class="text-decoration-none">Inicio</a></li>
-                        <li><a href="#" class="text-decoration-none">Explorar</a></li>
-                        <li><a href="#" class="text-decoration-none">Contacto</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <h5>Síguenos</h5>
-                    <div class="social-links">
-                        <a href="#" class="text-decoration-none me-2"><i class="bi bi-facebook"></i></a>
-                        <a href="#" class="text-decoration-none me-2"><i class="bi bi-twitter"></i></a>
-                        <a href="#" class="text-decoration-none me-2"><i class="bi bi-instagram"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="row mt-3">
-                <div class="col-12 text-center">
                     <small>&copy; 2024 Tu Plataforma. Todos los derechos reservados.</small>
                 </div>
             </div>
         </div>
-    </footer>
+    </div>
+</footer>
 
-    <!-- Bootstrap JavaScript -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    
+<!-- Bootstrap JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </body>
 </html>
